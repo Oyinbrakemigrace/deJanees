d564d211053894841bac0a887e88fcf7a56cc6ba
Oyinbrakemigrace <graceofubu04@gmail.com>
2025-09-22 08:33:38 +0100

    navbar and hero section

--- src/components/Home Components/HeroSection.jsx
+++ src/components/Home Components/HeroSection.jsx
@@ -4,13 +4,13 @@ import { Link } from "react-router-dom";
 
 const HeroSection = () => {
   return (
-    <div className="font-sans bg-[url(./assets/panel.jpg)] pt-[7rem] bg-cover bg-center bg-no-repeat h-screen relative">
-      <div className="absolute top-0 h-full w-full bg-black/70" />
+    <div className="bg-[url(./assets/panel.jpg)] pt-[7rem] bg-cover bg-center bg-no-repeat h-screen relative">
+      <div className="absolute top-0 h-full w-full bg-black/50" />
       <div className="absolute lg:p-0 text-slate-100 inset-0 flex flex-col justify-center lg:items-center backdrop-blur-[2px] my-auto">
-        <h2 className="md:text-[5.5rem] text-[2.5rem] text-center md:leading-tight text-slate-100 md:mt-20 mt-10 font-extrabold">
+        <h2 className="md:text-[5.5rem] text-[2.2rem] text-center md:leading-tight text-slate-100 md:mt-20 mt-10 font-extrabold">
           Powering Tomorrow, Today!
         </h2>
-        <h5 className="lg:text-2xl text-xl text-center italic mt-4">
+        <h5 className="lg:text-2xl text-lg text-center italic mt-4">
           Clean, reliable, and sustainable energy solutions for homes and
           businesses.
         </h5>
@@ -18,7 +18,7 @@ const HeroSection = () => {
           <button>
             <Link
               to="/our-projects"
-              className="bg-transparent border-2 border-company-blue hover:bg-company-blue/50 rounded-md lg:px-10 lg:py-4 px-5 py-3"
+              className="bg-transparent border-2 md:text-2xl text-sm border-company-blue hover:bg-company-blue/50 rounded-md lg:px-10 lg:py-4 px-5 py-3"
             >
               Explore Projects
             </Link>
