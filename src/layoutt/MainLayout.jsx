e38eb5eb4065792d0110e810dbc1dac6b4c93d43
Oyinbrakemigrace <graceofubu04@gmail.com>
2025-09-23 16:41:59 +0100

    fix

--- src/layoutt/MainLayout.jsx
+++ src/layoutt/MainLayout.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { Outlet, useLocation } from 'react-router-dom'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 
 const MainLayout = () => {
+    const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname])
+
+
+
   return (
     <div>
       <header>
